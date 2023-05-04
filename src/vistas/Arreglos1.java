a75b5a77c94defb57ad471284a1c49df5e32cb24
Your Name <you@example.com>
2023-05-04 18:12:52 -0500

    ejercicios java v1

---
+++ src/vistas/Arreglos1.java
@@ -0,0 +1,15 @@
+package vistas;
+public class Arreglos1 {
+    
+    public static void main(String[] args){
+        
+        String nombres[]={"Pablo","Pedro","Ana"
+        ,"Luis","Arturo","Jose","Andre"};
+        
+        for(int i=0;i<nombres.length;i++){
+            if(nombres[i].charAt(0)=='A'){
+                System.out.println(nombres[i]);
+            }
+        }
+    }
+}
