2baebb4d6384b1a82aa89044c1312a2ced2422a5
Your Name <you@example.com>
2023-05-09 11:44:14 -0500

    ejercicio calcular edad v2

--- src/vistas/CalculoEdad.java
+++ src/vistas/CalculoEdad.java
@@ -4,6 +4,9 @@
  */
 package vistas;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 import java.util.Calendar;
 
 /**
@@ -36,6 +39,7 @@ public class CalculoEdad extends javax.swing.JFrame {
         procesar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         txtresultado = new javax.swing.JTextArea();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -45,7 +49,7 @@ public class CalculoEdad extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel1.setText("Calculo de Edad");
 
-        jLabel2.setText("Año Nacimiento:");
+        jLabel2.setText("Fecha Nacimiento:");
 
         procesar.setText("Procesar");
         procesar.addActionListener(new java.awt.event.ActionListener() {
@@ -58,22 +62,26 @@ public class CalculoEdad extends javax.swing.JFrame {
         txtresultado.setRows(5);
         jScrollPane1.setViewportView(txtresultado);
 
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel3.setText("formato fecha: (año-mes-dia)");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(21, 21, 21)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel1)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(procesar, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txtañonacimiento, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addComponent(jScrollPane1))
-                .addContainerGap(25, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel3)
+                    .addComponent(procesar, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jLabel1)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                            .addComponent(jLabel2)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(txtañonacimiento, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jScrollPane1)))
+                .addContainerGap(16, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -84,11 +92,13 @@ public class CalculoEdad extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(txtañonacimiento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(procesar)
+                .addGap(2, 2, 2)
+                .addComponent(jLabel3)
                 .addGap(18, 18, 18)
+                .addComponent(procesar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(29, Short.MAX_VALUE))
+                .addContainerGap(17, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -107,16 +117,18 @@ public class CalculoEdad extends javax.swing.JFrame {
 
     private void procesarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_procesarActionPerformed
         // TODO add your handling code here:
-        int añoactual,añoNacimiento,edad;
+        String Nacimiento;
         
-        añoNacimiento=Integer.parseInt(txtañonacimiento.getText());
+        Nacimiento=txtañonacimiento.getText();
         
-        Calendar fec=Calendar.getInstance();
-        añoactual=fec.get(Calendar.YEAR);
+        LocalDate fechaNacimiento=LocalDate.parse(Nacimiento);
+        LocalDate fechaActual=LocalDate.now();
         
-        edad=añoactual-añoNacimiento;
+        Period periodo=Period.between(fechaNacimiento, fechaActual);
         
-        txtresultado.setText("Tu edad es de: "+edad+" años");
+        txtresultado.setText("Tienes: "+periodo.getYears()+" años\n"
+                + "con "+periodo.getMonths()+" Meses\n"
+                + "y "+periodo.getDays()+" dias");
     }//GEN-LAST:event_procesarActionPerformed
 
     /**
@@ -157,6 +169,7 @@ public class CalculoEdad extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton procesar;
