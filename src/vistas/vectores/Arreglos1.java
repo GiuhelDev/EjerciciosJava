2d64778776277abf8a2687eefe71f615a90e5064
Your Name <you@example.com>
2023-05-18 12:09:03 -0500

    ejercicio 1 de vectores

--- src/vistas/vectores/Arreglos1.java
+++ src/vistas/vectores/Arreglos1.java
@@ -1,4 +1,4 @@
-package vistas;
+package vistas.vectores;
 public class Arreglos1 {
     
     public static void main(String[] args){
---
+++ src/vistas/vectores/vectores1.java
@@ -0,0 +1,23 @@
+package vistas.vectores;
+import java.util.Scanner;
+public class vectores1 {
+    
+    public static void main(String[] args){
+        
+        Scanner entrada=new Scanner(System.in);
+        int [] numero=new int[5];
+        
+        System.out.println("Ingrese 5 numero: ");
+        
+        for(int i=0;i<5;i++){
+            numero[i]=entrada.nextInt();
+        }
+        for(int i=0;i<5;i++){
+            if(numero[i]%2==0){
+                System.out.println(numero[i]+" es Par");
+            }else{
+                 System.out.println(numero[i]+" es inPar");
+            }
+        }
+    }
+}
